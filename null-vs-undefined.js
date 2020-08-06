2d56209dbb6f80808160e1959174f6457377331e
devabu23 <devabu23>
2020-08-07 02:42:19 +0600

    null-vs-undefined.js

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,10 +1,10 @@
-// function name(num1, num2) {
-//     console.log(num1, num2);
-//     return 
-// }
+function name(num1, num2) {
+    console.log(num1, num2);
+    return 
+}
 
-// const info = {name: "jack", phone: 8876888};
-// console.log(info.address);
+const info = {name: "jack", phone: 8876888};
+console.log(info.address);
 
 let fun = undefined;
 console.log(fun);

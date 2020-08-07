a31d8af25d56371eeb9ab671a874eb057a4948f6
devabu23 <devabu23>
2020-08-08 01:37:38 +0600

    scope.js

---
+++ scope.js
@@ -0,0 +1,15 @@
+let bouns = 20 ;
+function sum(first, second) {
+    let result = first + second + bouns;
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = 'funky';
+        mood = "cranky"
+
+        // console.log(mood);
+    }
+    return result
+}
+const output = sum(5, 6)
+console.log(output);
